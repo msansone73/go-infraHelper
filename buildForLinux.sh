53b3a98032661405b043759b93e2eac068bbe216
marcio sansone <msansone@gmail.com>
2020-06-11 15:10:38 +0100

    - In build script removed go get gin and fix build for several go files

--- buildForLinux.sh
+++ buildForLinux.sh
@@ -1,7 +1,7 @@
-echo "get gin package"
-go get -u github.com/gin-gonic/gin
+#echo "get gin package"
+#go get -u github.com/gin-gonic/gin
 echo "build "
-go build main.go
+go build *.go
 echo "move for bin folder"
 mv main /bin
 echo "finish."
