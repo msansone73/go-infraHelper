ef1e442f7bf46421353a6b6bce11ea72900bcc73
marcio sansone <msansone@gmail.com>
2020-06-12 23:10:42 +0100

    added error handler

--- main.go
+++ main.go
@@ -14,6 +14,9 @@ func main()  {
 	r := gin.Default()
 	setRotas(r)
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	err := r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	if (err!=nil){
+		fmt.Println("Erro = ", err)
+	}
 	
 }
