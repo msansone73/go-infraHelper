79e865a19af4690f61c420e8d1b0e4f5002812b2
marcio sansone <msansone@gmail.com>
2020-06-12 05:38:35 +0100

    - Added SSL

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"projects/go-infraHelper/parametro"
 	"github.com/gin-gonic/autotls"
-	"golang.org/x/crypto/acme/autocert"
+	//"golang.org/x/crypto/acme/autocert"
 	"github.com/gin-gonic/gin"
 	"log"
 )
@@ -18,13 +18,14 @@ func main()  {
 	r := gin.Default()
 	setRotas(r)
 
-	m := autocert.Manager{
-		Prompt:     autocert.AcceptTOS,
-		HostPolicy: autocert.HostWhitelist("msansone.com.br"),
-		Cache:      autocert.DirCache("/var/www/.cache"),
-	}
+	// m := autocert.Manager{
+	// 	Prompt:     autocert.AcceptTOS,
+	// 	HostPolicy: autocert.HostWhitelist("msansone.com.br"),
+	// 	Cache:      autocert.DirCache("/var/www/.cache"),
+	// }
 
-	log.Fatal(autotls.RunWithManager(r, &m))
+	//log.Fatal(autotls.RunWithManager(r, &m))
+	log.Fatal(autotls.Run(r, "msansone.com.br"))
 
 	//r.Run(":443") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	
